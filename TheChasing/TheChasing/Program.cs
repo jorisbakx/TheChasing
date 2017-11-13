645d4691e89cb1dbad3a38c28ec6aa294b01e644
Andries <nieu0082@hz.nl>
2017-11-13 16:18:28 +0100

    flow-comments added

--- TheChasing/TheChasing/Program.cs
+++ TheChasing/TheChasing/Program.cs
@@ -6,8 +6,14 @@ namespace TheChasing
     {
         static void Main(string[] args)
         {
+            // make jail
             Jail jail = new Jail();
 
+            // make thief
+
+            // make cop
+
+            // cop puts thief in jail
             Console.WriteLine(jail);
 
             Console.Read();
